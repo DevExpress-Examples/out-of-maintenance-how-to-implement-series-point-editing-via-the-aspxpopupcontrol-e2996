3ebc88db24b020d0155d65d02857045f50a12c17
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/App_Code/BO.vb
@@ -0,0 +1,46 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Web
+
+Public Class ManualDataSet
+	Inherits DataSet
+	Public Sub New()
+		MyBase.New()
+		Dim table As New DataTable("table")
+
+		DataSetName = "ManualDataSet"
+
+		table.Columns.Add("ID", GetType(Int32))
+		table.Columns.Add("MyDateTime", GetType(DateTime))
+		table.Columns.Add("MyRow", GetType(String))
+		table.Columns.Add("MyData", GetType(Double))
+		table.Constraints.Add("IDPK", table.Columns("ID"), True)
+
+		Tables.AddRange(New DataTable() { table })
+	End Sub
+
+	Public Shared Function CreateData() As DataSet
+		Dim ds As DataSet = New ManualDataSet()
+		Dim table As DataTable = ds.Tables("table")
+
+		table.Rows.Add(New Object() { 0, DateTime.Today.AddHours(1), "A", 103 })
+		table.Rows.Add(New Object() { 1, DateTime.Today.AddHours(3), "B", 200 })
+		table.Rows.Add(New Object() { 2, DateTime.Today.AddHours(6), "C", 446 })
+		table.Rows.Add(New Object() { 3, DateTime.Today.AddHours(10), "D", 788 })
+		table.Rows.Add(New Object() { 4, DateTime.Today.AddHours(15), "E", 674 })
+		table.Rows.Add(New Object() { 5, DateTime.Today.AddHours(20), "F", 452 })
+
+		Return ds
+	End Function
+
+	Public Shared Function RetrieveDataCache() As DataSet
+		If HttpContext.Current.Session("dataset") Is Nothing Then
+			HttpContext.Current.Session("dataset") = ManualDataSet.CreateData()
+		End If
+
+		Return CType(HttpContext.Current.Session("dataset"), DataSet)
+	End Function
+
+End Class
+
---
+++ VB/WebSite/ClientScripts.js
@@ -0,0 +1,27 @@
+﻿function Chart_ObjectHotTracked(s, e) {
+    if (e.hitInfo.inSeriesPoint && !e.hitInfo.inSeriesLabel)
+        chart.SetCursor('hand');
+    else
+        chart.SetCursor('');
+}
+
+function Chart_ObjectSelected(s, e) {
+    if (e.hitInfo.inSeriesPoint && !e.hitInfo.inSeriesLabel) {
+        var selectedPoint = e.additionalHitObject;
+
+        document.getElementById('hfOriginalArgument').value = _aspxGetInvariantDateTimeString(_aspxToUtcTime(selectedPoint.argument));
+        teArgument.SetDate(_aspxToUtcTime(selectedPoint.argument));
+        tbValue.SetText(selectedPoint.values[0]);
+
+        popup.Show();
+    }
+}
+
+function Ok_Click(s, e) {
+    popup.Hide();
+    chart.PerformCallback(_aspxGetInvariantDateTimeString(teArgument.GetDate()) + ';' + tbValue.GetText());
+}
+
+function Cancel_Click(s, e) {
+    popup.Hide();
+}
