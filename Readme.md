c4941008b5626a73860f21d7455187db31c72af5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to implement series point editing via the ASPxPopupControl
+
+
+<p>This example illustrates how to implement series point editing via the ASPxPopupControl. For this, the <a href="http://documentation.devexpress.com/#XtraCharts/DevExpressXtraChartsWebScriptsASPxClientWebChartControl_ObjectSelectedtopic"><u>ASPxClientWebChartControl.ObjectSelected Event</u></a> is handled. In this event handler a series point argument and value are assigned to editors (ASPxTextBox and ASPxTimeEdit in this example) within a popup, and this popup is displayed via the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxPopupControlScriptsASPxClientPopupControl_Showtopic"><u>ASPxClientPopupControl.Show Method</u></a><u>.</u> When the editing is finished (in the "Ok" button click event handler), modified values are passed to the server side (via the <a href="http://documentation.devexpress.com/#XtraCharts/DevExpressXtraChartsWebScriptsASPxClientWebChartControl_PerformCallbacktopic"><u>ASPxClientWebChartControl.PerformCallback Method</u></a>) to update the underlying datasource to which the WebChartControl is bound.</p><p>Also, please note the way, in which time values in Local format are converted to UTC format and to string format which can be parsed on the server side via the <a href="http://msdn.microsoft.com/en-us/library/9xk1h71t.aspx"><u>Convert.ToDateTime Method</u></a>. For this, the _aspxToUtcTime and _aspxGetInvariantDateTimeString methods are used.</p><p><strong>See also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/E2466">How to post data back to the database when editing series points in the WebChartControl</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,56 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Globalization
+Imports DevExpress.XtraCharts
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		BindChartToData()
+	End Sub
+
+	Private Sub BindChartToData()
+		' Create a series
+		Dim series As New Series("Series", ViewType.Line)
+
+		series.ArgumentScaleType = ScaleType.DateTime
+		series.ValueScaleType = ScaleType.Numerical
+
+		CType(series.View, LineSeriesView).LineMarkerOptions.Size = 20
+
+		' Bind series to data
+		series.DataSource = ManualDataSet.RetrieveDataCache().Tables(0)
+		series.ArgumentDataMember = "MyDateTime"
+		series.ValueDataMembers.AddRange(New String() { "MyData" })
+		WebChartControl1.Series.Add(series)
+
+		' Adjust X axis options
+		CType(WebChartControl1.Diagram, XYDiagram).AxisX.DateTimeOptions.Format = DateTimeFormat.Custom
+		CType(WebChartControl1.Diagram, XYDiagram).AxisX.DateTimeOptions.FormatString = "HH:mm:ss"
+		CType(WebChartControl1.Diagram, XYDiagram).AxisX.Label.Staggered = True
+
+		CType(WebChartControl1.Diagram, XYDiagram).AxisX.DateTimeMeasureUnit = DateTimeMeasurementUnit.Hour
+		'((XYDiagram)WebChartControl1.Diagram).AxisX.GridSpacingAuto = false;
+		'((XYDiagram)WebChartControl1.Diagram).AxisX.GridSpacing = 1;
+
+		WebChartControl1.DataBind()
+	End Sub
+
+	Protected Sub WebChartControl1_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.XtraCharts.Web.CustomCallbackEventArgs)
+		Dim parameters() As String = e.Parameter.Split(";"c)
+		Dim originalArgument As DateTime = Convert.ToDateTime(hfOriginalArgument.Value, CultureInfo.InvariantCulture)
+		Dim argument As DateTime = Convert.ToDateTime(parameters(0), CultureInfo.InvariantCulture)
+		Dim value As Double = Convert.ToDouble(parameters(1))
+		Dim table As DataTable = ManualDataSet.RetrieveDataCache().Tables(0)
+
+		For i As Integer = 0 To table.Rows.Count - 1
+			If Convert.ToDateTime(table.Rows(i)("MyDateTime")) = originalArgument Then
+				table.Rows(i)("MyDateTime") = argument
+				table.Rows(i)("MyData") = value
+				Exit For
+			End If
+		Next i
+	End Sub
+
+End Class
